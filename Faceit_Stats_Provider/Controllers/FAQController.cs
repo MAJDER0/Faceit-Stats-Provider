360686b28d7ba753408e034b5b57aa8fe33b7df3
MAJDER0 <tomasz.majder89@gmail.com>
2024-06-20 22:40:28 +0200

    FAQ Controller

---
+++ Faceit_Stats_Provider/Controllers/FAQController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Faceit_Stats_Provider.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/FAQ/FAQ.cshtml");
+        }
+    }
+}
