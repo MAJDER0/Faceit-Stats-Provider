c722cc36176f2c01cfbae95ef9c6b506eb9bb19e
MAJDER0 <tomasz.majder89@gmail.com>
2024-06-29 00:24:45 +0200

    Deleting unused data

--- Faceit_Stats_Provider/Models/MatchHistory.cs
+++ Faceit_Stats_Provider/Models/MatchHistory.cs
@@ -5,10 +5,6 @@
         public class Rootobject
         {
             public Item[] items { get; set; }
-            public int start { get; set; }
-            public int end { get; set; }
-            public int from { get; set; }
-            public int to { get; set; }
         }
 
         public class Item
@@ -16,21 +12,13 @@
             public string match_id { get; set; }
             public string game_id { get; set; }
             public string region { get; set; }
-            public string match_type { get; set; }
-            public string game_mode { get; set; }
-            public int max_players { get; set; }
-            public int teams_size { get; set; }
+            public string match_type { get; set; }          
             public Teams teams { get; set; }
             public string[] playing_players { get; set; }
             public string competition_id { get; set; }
             public string competition_name { get; set; }
             public string competition_type { get; set; }
-            public string organizer_id { get; set; }
-            public string status { get; set; }
-            public int started_at { get; set; }
-            public int finished_at { get; set; }
             public Results results { get; set; }
-            public string faceit_url { get; set; }
         }
 
         public class Teams
@@ -44,7 +32,6 @@
             public string team_id { get; set; }
             public string nickname { get; set; }
             public string avatar { get; set; }
-            public string type { get; set; }
             public Player[] players { get; set; }
         }
 
@@ -56,7 +43,6 @@
             public int skill_level { get; set; }
             public string game_player_id { get; set; }
             public string game_player_name { get; set; }
-            public string faceit_url { get; set; }
         }
 
         public class Faction1
@@ -76,12 +62,10 @@
             public int skill_level { get; set; }
             public string game_player_id { get; set; }
             public string game_player_name { get; set; }
-            public string faceit_url { get; set; }
         }
 
         public class Results
         {
-            public string winner { get; set; }
             public Score score { get; set; }
         }
 
