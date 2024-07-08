46065c366a1535bfa7063300fd1d23e84892447a
MAJDER0 <tomasz.majder89@gmail.com>
2024-07-08 21:11:38 +0200

    Extension n Analyzer Feature Tabs

---
+++ Faceit_Stats_Provider/Controllers/AnalyzerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Faceit_Stats_Provider.Controllers
+{
+    public class AnalyzerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Analyzer/Analyzer.cshtml");
+        }
+    }
+}
---
+++ Faceit_Stats_Provider/Controllers/ExtensionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Faceit_Stats_Provider.Controllers
+{
+    public class ExtensionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Extension/Extension.cshtml");
+        }
+    }
+}
