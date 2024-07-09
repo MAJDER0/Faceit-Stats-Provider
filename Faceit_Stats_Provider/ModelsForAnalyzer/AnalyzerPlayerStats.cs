f5bfa8d42c76b9a69a37e1e397080ebcd331cf25
MAJDER0 <tomasz.majder89@gmail.com>
2024-07-09 16:00:01 +0200

    Map Statistics For Each Player From Lobby

--- Faceit_Stats_Provider/ModelsForAnalyzer/AnalyzerPlayerStats.cs
+++ Faceit_Stats_Provider/ModelsForAnalyzer/AnalyzerPlayerStats.cs
@@ -1,4 +1,6 @@
-﻿namespace Faceit_Stats_Provider.ModelsForAnalyzer
+﻿using System.Text.Json.Serialization;
+
+namespace Faceit_Stats_Provider.ModelsForAnalyzer
 {
     public class AnalyzerPlayerStats
     {
@@ -27,42 +29,55 @@
 
         public class Segment
         {
-            public string type { get; set; }
-            public string mode { get; set; }
             public string label { get; set; }
+            [JsonPropertyName("img_small")]
             public string img_small { get; set; }
+            [JsonPropertyName("img_regular")]
             public string img_regular { get; set; }
             public Stats stats { get; set; }
+            public string type { get; set; }
+            public string mode { get; set; }
         }
 
         public class Stats
         {
-            public string AverageKills { get; set; }
-            public string AverageKDRatio { get; set; }
-            public string Headshots { get; set; }
-            public string PentaKills { get; set; }
-            public string TotalHeadshots { get; set; }
-            public string AverageMVPs { get; set; }
-            public string Wins { get; set; }
-            public string MVPs { get; set; }
-            public string WinRate { get; set; }
-            public string AverageHeadshots { get; set; }
             public string Kills { get; set; }
-            public string Assists { get; set; }
-            public string AverageAssists { get; set; }
-            public string Deaths { get; set; }
-            public string AverageDeaths { get; set; }
-            public string AverageKRRatio { get; set; }
-            public string HeadshotsperMatch { get; set; }
-            public string AverageTripleKills { get; set; }
-            public string TripleKills { get; set; }
-            public string Matches { get; set; }
-            public string KRRatio { get; set; }
-            public string AveragePentaKills { get; set; }
-            public string KDRatio { get; set; }
-            public string AverageQuadroKills { get; set; }
             public string QuadroKills { get; set; }
+            public string TripleKills { get; set; }
+            public string MVPs { get; set; }
+            [JsonPropertyName("Average Triple Kills")]
+            public string AverageTripleKills { get; set; }
+            public string PentaKills { get; set; }
+            public string AverageMVPs { get; set; }
+            [JsonPropertyName("Average Headshots %")]
+            public string AverageHeadshots { get; set; }
+            public string Assists { get; set; }
+            [JsonPropertyName("Average Kills")]
+            public string AverageKills { get; set; }
+            [JsonPropertyName("Headshots per Match")]
+            public string HeadshotsperMatch { get; set; }
+            [JsonPropertyName("Average K/R Ratio")]
+            public string AverageKRRatio { get; set; }
+            [JsonPropertyName("Average Quadro Kills")]
+            public string AverageQuadroKills { get; set; }
+            public string Matches { get; set; }
+            [JsonPropertyName("Win Rate %")]
+            public string WinRate { get; set; }
             public string Rounds { get; set; }
+            public string TotalHeadshots { get; set; }
+            public string KRRatio { get; set; }
+            public string Deaths { get; set; }
+            public string KDRatio { get; set; }
+            [JsonPropertyName("Average Assists")]
+            public string AverageAssists { get; set; }
+            [JsonPropertyName("Average Penta Kills")]
+            public string AveragePentaKills { get; set; }
+            public string Headshots { get; set; }
+            public string Wins { get; set; }
+            [JsonPropertyName("Average Deaths")]
+            public string AverageDeaths { get; set; }
+            [JsonPropertyName("Average K/D Ratio")]
+            public string AverageKDRatio { get; set; }
         }
 
     }
