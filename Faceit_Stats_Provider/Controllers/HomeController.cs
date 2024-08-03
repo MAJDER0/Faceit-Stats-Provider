e939f6a33de2821ae7daefa128abc42d98909e16
MAJDER0 <tomasz.majder89@gmail.com>
2024-08-03 20:32:55 +0200

    Fill in search bar with example data

--- Faceit_Stats_Provider/Controllers/HomeController.cs
+++ Faceit_Stats_Provider/Controllers/HomeController.cs
@@ -8,15 +8,18 @@ namespace Faceit_Stats_Provider.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
             ViewData["PlayerStats"] = true;
+            ViewBag.FaceitAPIKey = _configuration["FaceitAPI"];
             return View();
         }
 
---
+++ Faceit_Stats_Provider/Models/NicknamesToFill.cs
@@ -0,0 +1,10 @@
+﻿namespace Faceit_Stats_Provider.Models
+{
+    public class NicknamesToFill
+    {
+        public class Rootobject
+        {
+            public string nickname { get; set; }
+        }
+    }
+}
