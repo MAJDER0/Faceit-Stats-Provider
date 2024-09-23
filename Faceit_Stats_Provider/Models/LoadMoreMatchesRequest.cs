1f62d7bf073883dc2bcb173b44b8f1b0fe7a2305
MAJDER0 <tomasz.majder89@gmail.com>
2024-09-23 02:15:24 +0200

    Improved pagination n changed scrollbar color

--- Faceit_Stats_Provider/Models/LoadMoreMatchesRequest.cs
+++ Faceit_Stats_Provider/Models/LoadMoreMatchesRequest.cs
@@ -7,7 +7,7 @@
         public string playerID { get; set; }
         public bool isOffsetModificated { get; set; }
         public int QuantityOfEloRetrieves { get; set; }
-        public List<EloDiff.Root> currentModel { get; set; }  // Change from string to List<EloDiff.Root>
+        public List<EloDiff.Root> currentModel { get; set; }  
         public int currentPage { get; set; }
         public int CsGoSwap { get; set; } 
         public string Game { get; set; }
